1449532a2b135981fc58da5a3ea67e668b6f2326
Jeet Ghosh <ghoshjeet455@gmail.com>
2025-11-22 20:05:59 +0530

    updated

--- client/src/components/admin-view/productTile.jsx
+++ client/src/components/admin-view/productTile.jsx
@@ -15,7 +15,7 @@ function AdminProductTile({
           <img
             src={product?.image}
             alt={product?.title}
-            className="w-full h-[300px] object-cover rounded-t-xl"
+            className="w-full h-[300px] object-cover rounded-t-lg"
           />
         </div>
         <CardContent>
