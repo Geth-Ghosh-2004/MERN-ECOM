5f0d6cf7e25407d677644a70ed9663ca22e961c0
Jeet Ghosh <ghoshjeet455@gmail.com>
2025-12-16 12:31:15 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ Proper CORS config
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-ecom-j834.vercel.app",
     credentials: true,
   })
 );
