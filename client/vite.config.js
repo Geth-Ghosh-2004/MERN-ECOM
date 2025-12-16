781e66c08ed8befbe20757013c67f953371f0390
Jeet Ghosh <ghoshjeet455@gmail.com>
2025-12-16 13:59:14 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // your backend server
+        target: "https://mern-ecom-3-l9sy.onrender.com", // your backend server
         changeOrigin: true,
         secure: false,
       },
